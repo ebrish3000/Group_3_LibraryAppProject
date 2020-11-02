c1ad5cc13b5898221ebd56c1d59382f3968e49c6
nikolaM500 <nikola.markovic2014@gmail.com>
2020-11-01 20:27:33 -0700

    LoginPage

--- src/test/java/com/group_3/libraryApp/utilities/BrowserUtils.java
+++ src/test/java/com/group_3/libraryApp/utilities/BrowserUtils.java
@@ -14,6 +14,7 @@ import java.util.function.Function;
 import static org.junit.Assert.assertTrue;
 
 public class BrowserUtils {
+    private static WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
 
     /*
     Applies thread sleep without having to throw interrupted exception
@@ -254,6 +255,15 @@ public class BrowserUtils {
     public static void clickOnElement(WebElement element) {
         wait.until(ExpectedConditions.elementToBeClickable(element)).click();
     }
+
+    public static void enterText(WebElement element, String text){
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(text);
+        wait.until(ExpectedConditions.attributeToBe(element, "value", text));
+        System.out.println("Entering text: " + text);
+    }
+
 
 
 }
